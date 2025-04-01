5128db6ca4112715e320454afde006cd36f72efd
Gaurav Pratap Singh <gs492979@gmail.com>
2025-04-01 14:07:52 +0530

    gsap animation also added in mobile nav link

--- script.js
+++ script.js
@@ -2,7 +2,7 @@ const showMobileNavBtn = document.getElementById("mobile-nav-btn");
 const mobileNav = document.getElementById("mobile-nav-links");
 const logo = document.getElementById("logo");
 const desktopNavLink = document.querySelectorAll("#desktop-nav-links a");
-console.log(desktopNavLink);
+const mobileNavLink = document.querySelectorAll("#mobile-nav-links a");
 
 showMobileNavBtn.addEventListener("click", (e) => {
   mobileNav.classList.toggle("hidden");
@@ -12,12 +12,25 @@ const tl = gsap.timeline();
 tl.from(logo, {
   opacity: 0,
   duration: 0.7,
-  delay: 1,
+  delay: 0.6,
   y: -30,
 });
+
+tl.from(showMobileNavBtn, {
+  opacity: 0,
+  y: -30,
+  duration: 0.7,
+});
+
 tl.from(desktopNavLink, {
   opacity: 0,
   duration: 0.6,
+  y: -30,
+  stagger: 0.5,
+});
+tl.from(mobileNavLink, {
+  opacity: 0,
+  duration: 0.4,
   y: -30,
   stagger: 0.5,
 });
