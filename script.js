1f8ecd73cab64a13584994ce57f9d20e3e4b1cff
Gaurav Pratap Singh <gs492979@gmail.com>
2025-04-01 14:14:59 +0530

    animation added in main heading and arrow.

--- script.js
+++ script.js
@@ -3,34 +3,49 @@ const mobileNav = document.getElementById("mobile-nav-links");
 const logo = document.getElementById("logo");
 const desktopNavLink = document.querySelectorAll("#desktop-nav-links a");
 const mobileNavLink = document.querySelectorAll("#mobile-nav-links a");
+const mainHeading = document.getElementById("main-heading");
+const headerArrow = document.getElementById("header-arrow");
 
 showMobileNavBtn.addEventListener("click", (e) => {
   mobileNav.classList.toggle("hidden");
 });
 
-const tl = gsap.timeline();
-tl.from(logo, {
+const tl1 = gsap.timeline();
+const tl2 = gsap.timeline();
+tl1.from(logo, {
   opacity: 0,
   duration: 0.7,
   delay: 0.6,
   y: -30,
 });
 
-tl.from(showMobileNavBtn, {
+tl1.from(showMobileNavBtn, {
   opacity: 0,
   y: -30,
   duration: 0.7,
 });
 
-tl.from(desktopNavLink, {
+tl1.from(desktopNavLink, {
   opacity: 0,
   duration: 0.6,
   y: -30,
   stagger: 0.5,
 });
-tl.from(mobileNavLink, {
+tl1.from(mobileNavLink, {
   opacity: 0,
   duration: 0.4,
   y: -30,
   stagger: 0.5,
+});
+tl2.from(mainHeading, {
+  opacity: 0,
+  scale: 0.7,
+  duration: 0.68,
+  delay: 2,
+});
+tl2.from(headerArrow, {
+  opacity: 0,
+  scale: 0.7,
+
+  duration: 0.68,
 });
